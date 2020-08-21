59584874d8700b1c7de1ea7867219295b114e7d8
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-08-21 13:32:16 +0300

    some improvments

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
 
-const OrderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients)
-		.map(igKey => {
-			return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>
-		})
+class OrderSummary extends React.PureComponent {
+	componentDidUpdate() {
+		console.log('[Order summary] will update')
+	}
 
-	return (
-		<Aux>
-			<h3>Your order</h3>
-			<p>Delicios burger with the following ingredients</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p><strong>Total Price: {props.orderPrice.toFixed(2)} $</strong></p>
-			<p>Continue checkout</p>
-			<Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-			<Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-		</Aux>
-	)
+	render() {
+
+		const ingredientSummary = Object.keys(this.props.ingredients)
+			.map(igKey => {
+				return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+			})
+
+		return (
+			<Aux>
+				<h3>Your order</h3>
+				<p>Delicios burger with the following ingredients</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p><strong>Total Price: {this.props.orderPrice.toFixed(2)} $</strong></p>
+				<p>Continue checkout</p>
+				<Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+			</Aux>
+		)
+	}
+
+
 }
 
 export default OrderSummary;
