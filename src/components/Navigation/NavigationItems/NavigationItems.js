fe91e18d515673365c0a043e3870c1952a6e18ee
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-09-15 10:50:22 +0300

    start implementing auth logic. Auth component added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,17 +5,9 @@ import NavigationItem from './NavigationItem/NavigationItem'
 const NavigationItems = (props) => {
 	return (
 		<ul className={classes.NavigationItems}>
-			
-			<NavigationItem
-				link="/" exact >
-				Buirger Builder
-			</NavigationItem>
-
-			<NavigationItem
-				link="/orders">
-				Orders
-			</NavigationItem>
-
+			<NavigationItem	link="/" exact >Buirger Builder</NavigationItem>
+			<NavigationItem link="/orders">Orders</NavigationItem>
+			<NavigationItem link="/auth">Authenticate</NavigationItem>
 		</ul>
 	)
 }
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -114,6 +114,9 @@ class ContactData extends React.Component {
 
 	checkValidation(value, rules) {
 		let isValid = true;
+		if (!rules) {
+			return true;
+		}
 
 		if (rules.required) {
 			isValid = value.trim() !== '' && isValid;
@@ -126,6 +129,17 @@ class ContactData extends React.Component {
 		if (rules.maxLength) {
 			isValid = value.length <= rules.maxLength && isValid
 		}
+
+		if (rules.isEmail) {
+			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+			isValid = pattern.test(value) && isValid
+		}
+
+		if (rules.isNumeric) {
+			const pattern = /^\d+$/;
+			isValid = pattern.test(value) && isValid
+		}
+
 		return isValid;
 	}
 
